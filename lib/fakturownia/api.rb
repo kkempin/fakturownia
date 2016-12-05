b2ea0e44cb4c678e25cf39c453982f22968acbfe
Piotrek Bator <piotrek@nibynic.com>
2016-12-05 12:31:58 +0100

    Adds support for additional invoices params

--- lib/fakturownia/api.rb
+++ lib/fakturownia/api.rb
@@ -7,7 +7,7 @@ module Fakturownia
       endpoint = "https://#{Fakturownia.account_name}.fakturownia.pl/invoices/#{invoice_id}.json"
       response = self.make_get_request(endpoint)
 
-      (response.code == '200') ? JSON.parse(response.body) : nil 
+      (response.code == '200') ? JSON.parse(response.body) : nil
     end
 
     # Get JSON-formatted invoice based on order id
@@ -15,29 +15,38 @@ module Fakturownia
       endpoint = "https://#{Fakturownia.account_name}.fakturownia.pl/invoices.json"
       response = self.make_get_request(endpoint, { :oid => order_id })
 
-      (response.code == '200') ? JSON.parse(response.body) : nil 
+      (response.code == '200') ? JSON.parse(response.body) : nil
     end
 
     # Get JSON-formatted invoices based from given period
     # Available periods = [:all, :this_month, :last_month, :this_year, :last_year, :more]
     # If period = :more than date_from and date_to has to be set
-    def self.invoices period, date_from = nil, date_to = nil
-      endpoint = "https://#{Fakturownia.account_name}.fakturownia.pl/invoices.json?period=#{period}"
-      additional_params = { :period => period }
-      if period.to_s == 'more'
-        additional_params[:date_from] = date_from
-        additional_params[:date_to] = date_to
+    def self.invoices period_or_params, date_from = nil, date_to = nil
+      params = if period_or_params.is_a?(Hash)
+        period_or_params.symbolize_keys
+      else
+        {
+          period: period_or_params,
+          date_from: date_from,
+          date_to: date_to
+        }
       end
-      response = self.make_get_request(endpoint, additional_params)
 
-      (response.code == '200') ? JSON.parse(response.body) : nil 
+      endpoint = "https://#{Fakturownia.account_name}.fakturownia.pl/invoices.json"
+      if params[:period].to_s != "more"
+        params.delete(:date_from)
+        params.delete(:date_to)
+      end
+      response = self.make_get_request(endpoint, params)
+
+      (response.code == '200') ? JSON.parse(response.body) : nil
     end
 
     # Get invoice PDF based on invoice id
     def self.pdf invoice_id
       endpoint = "https://#{Fakturownia.account_name}.fakturownia.pl/invoices/#{invoice_id}.pdf"
       response = self.make_get_request(endpoint)
-       
+
       (response.code == '200') ? response.body : nil
     end
 
@@ -45,20 +54,20 @@ module Fakturownia
     # Create new invoice
     def self.create invoice_json
       endpoint = "https://#{Fakturownia.account_name}.fakturownia.pl/invoices.json"
-      uri = URI.parse(endpoint) 
+      uri = URI.parse(endpoint)
 
-      json_params = { 
+      json_params = {
         "api_token" => Fakturownia.api_token,
         "invoice" => invoice_json
       }
 
-      request = Net::HTTP::Post.new(uri.path) 
-      request.body = JSON.generate(json_params) 
-      request["Content-Type"] = "application/json" 
-      http = Net::HTTP.new(uri.host, uri.port) 
+      request = Net::HTTP::Post.new(uri.path)
+      request.body = JSON.generate(json_params)
+      request["Content-Type"] = "application/json"
+      http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      response = http.start {|h| h.request(request)} 
+      response = http.start {|h| h.request(request)}
       (response.code == '201') ? JSON.parse(response.body) : nil
     end
 
@@ -67,20 +76,20 @@ module Fakturownia
     # invoice_id - id of invoice from fakturownia
     def self.update invoice_id, invoice_json
       endpoint = "https://#{Fakturownia.account_name}.fakturownia.pl/invoices/#{invoice_id}.json"
-      uri = URI.parse(endpoint) 
+      uri = URI.parse(endpoint)
 
-      json_params = { 
+      json_params = {
         "api_token" => Fakturownia.api_token,
         "invoice" => invoice_json
       }
 
-      request = Net::HTTP::Put.new(uri.path) 
-      request.body = JSON.generate(json_params) 
-      request["Content-Type"] = "application/json" 
-      http = Net::HTTP.new(uri.host, uri.port) 
+      request = Net::HTTP::Put.new(uri.path)
+      request.body = JSON.generate(json_params)
+      request["Content-Type"] = "application/json"
+      http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      response = http.start {|h| h.request(request)} 
+      response = http.start {|h| h.request(request)}
 
       (response.code == '201') ? JSON.parse(response.body) : nil
     end
@@ -89,10 +98,10 @@ module Fakturownia
     # Delete invoice
     def self.delete invoice_id
       endpoint = "https://#{Fakturownia.account_name}.fakturownia.pl/invoices/#{invoice_id}.json"
-      uri = URI.parse(endpoint) 
+      uri = URI.parse(endpoint)
       whole_url = uri.path + "?api_token=#{Fakturownia.api_token}"
 
-      http = Net::HTTP.new(uri.host, uri.port) 
+      http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       http.delete(whole_url)
@@ -125,7 +134,7 @@ module Fakturownia
       (response.code == '200' and response.body.to_s != 'brak klienta') ? response.body : nil
     end
 
-    
+
     # Make GET request returning response
     def self.make_get_request endpoint, additional_params = {}
       uri = URI.parse(endpoint)
@@ -134,8 +143,8 @@ module Fakturownia
         whole_url += "&#{k}=#{v}"
       end
 
-      request = Net::HTTP::Get.new(whole_url) 
-      http = Net::HTTP.new(uri.host, uri.port) 
+      request = Net::HTTP::Get.new(whole_url)
+      http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       http.start {|h| h.request(request)}
